57d1a7fe0e799190340059aaf7929e752e995c18
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-12-31 04:53:50 -0300

    add Player class

---
+++ src/player.js
@@ -0,0 +1,12 @@
+class Player {
+
+    constructor(name = `Player${this.playerNum}` = 1) {
+        this.name = name;
+        this.playerNum = this.playerNum;
+    }
+
+}
+
+
+
+modules.export = Player;
