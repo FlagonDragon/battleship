e9bd8afbc789202717d398fe47790ac90b4224d5
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-01-11 23:24:40 -0300

    add coordinate visuals

--- src/DOM.js
+++ src/DOM.js
@@ -2,12 +2,31 @@ const { player1, player2 } = require('./player');
 
 const instruction = document.getElementById('instruction');
 const restartBtn = document.getElementById('restartBtn');
+const upperCoords1 = document.getElementById("upperCoords1");
+const upperCoords2 = document.getElementById("upperCoords2");
+const leftCoords1 = document.getElementById("leftCoords1");
+const leftCoords2 = document.getElementById("leftCoords2");
 const player1Name = document.getElementById("name1");
 const player2Name = document.getElementById("name2");
 
 instruction.textContent = 'Placeholder';
 player1Name.textContent = player1.name;
 player2Name.textContent = player2.name;
+
+function appendCoords(myDiv) {
+
+  for (let i = 0; i <= 9; i++) {
+    let div = document.createElement('div');
+    div.textContent = i;
+    myDiv.appendChild(div);
+  }
+
+};
+
+appendCoords(upperCoords1);
+appendCoords(upperCoords2);
+appendCoords(leftCoords1);
+appendCoords(leftCoords2);
 
 function refreshDOM(gameState, currentPlayer) {
 
